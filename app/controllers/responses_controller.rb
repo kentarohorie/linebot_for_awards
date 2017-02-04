447da665b87a0152e5c26066a08fe98b84e686b0
kentarohorie <kentaro.horie.4@gmail.com>
2017-02-04 17:26:17 +0900

    change

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,4 +1,5 @@
 class ResponsesController < ApplicationController
+  skip_before_filter :verify_authenticity_token
   CHANNEL_SECRET = "3f46876f880a741ee8dd0453ed433cd5"
   CHANNEL_ACCESS_TOKEN = "V1nZVaGmdYgECu+jOw5My+tbeV0DoApp5d1cPsA5cg2YqOLkLxOD6Bd4TiCFjF4vj6VyTlaS9+S+a3qY+Zpr5YJOLkABHyqhv+7W38IO2+z4cTPQeFTCp3BK2aVOTRxuIVVIP8sF8sXZbUF7PI0xtgdB04t89/1O/w1cDnyilFU="
 
