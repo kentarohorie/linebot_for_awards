8ca023c0870c7844c6f0cae5ff50b2efd8d43f65
kentarohorie <kentaro.horie.4@gmail.com>
2017-02-04 17:01:06 +0900

    change

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -3,6 +3,7 @@ class ResponsesController < ApplicationController
   CHANNEL_ACCESS_TOKEN = "V1nZVaGmdYgECu+jOw5My+tbeV0DoApp5d1cPsA5cg2YqOLkLxOD6Bd4TiCFjF4vj6VyTlaS9+S+a3qY+Zpr5YJOLkABHyqhv+7W38IO2+z4cTPQeFTCp3BK2aVOTRxuIVVIP8sF8sXZbUF7PI0xtgdB04t89/1O/w1cDnyilFU="
 
   def callback
+    puts "hgeohgoehgoehogoehgoehgoehgoehgoehgoekentarohorie"
     # event = params[:events][0]
     # event_type = event[:type]
     # reply_token = event[:replyToken]
