bb509192c08dd5dcb7cd05ceed606014baebed0a
kentarohorie <kentaro.horie.4@gmail.com>
2017-02-04 15:59:22 +0900

    change

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  puts "=~=~=~=~=~=~=~=~=~=~=~=~"
+  puts params
+  puts "=~=~=~=~=~=~=~=~=~=~=~=~"
   root 'responses#index'
   resources :responses, only: :index
 end
