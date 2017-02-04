2702200caa94b473ffcc7b35ee367cf6bb150047
kentarohorie <kentaro.horie.4@gmail.com>
2017-02-04 17:40:39 +0900

    change

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,4 +1,5 @@
 class ResponsesController < ApplicationController
+  require 'line/bot'
   skip_before_filter :verify_authenticity_token
   CHANNEL_SECRET = "3f46876f880a741ee8dd0453ed433cd5"
   CHANNEL_ACCESS_TOKEN = "V1nZVaGmdYgECu+jOw5My+tbeV0DoApp5d1cPsA5cg2YqOLkLxOD6Bd4TiCFjF4vj6VyTlaS9+S+a3qY+Zpr5YJOLkABHyqhv+7W38IO2+z4cTPQeFTCp3BK2aVOTRxuIVVIP8sF8sXZbUF7PI0xtgdB04t89/1O/w1cDnyilFU="
@@ -31,8 +32,6 @@ class ResponsesController < ApplicationController
       config.channel_secret = CHANNEL_SECRET
       config.channel_token = CHANNEL_ACCESS_TOKEN
     }
-    puts @client
-    binding.pry
   end
 end
 
