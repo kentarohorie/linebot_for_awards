b887a61976e40f4e3d7566c2379c0d12cc025487
kentarohorie <kentaro.horie.4@gmail.com>
2017-02-04 17:29:32 +0900

    change

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -5,33 +5,33 @@ class ResponsesController < ApplicationController
 
   def callback
     puts "hgeohgoehgoehogoehgoehgoehgoehgoehgoekentarohorie"
-    # event = params[:events][0]
-    # event_type = event[:type]
-    # reply_token = event[:replyToken]
-    # output_text = ""
-    # case event_type
-    # when "message"
-    #   input_text = event[:message][:text]
-    #   output_text = input_text
-    #   message = {
-    #     type: "text",
-    #     text: output_text
-    #   }
-    #   client.reply_message(reply_token, message)
-    # end
-
-    body = request.body.read
-    events = client.parse_events_from(body)
-    events.each do |event|
-      case event
-      when Line::Bot::Event::Message
-        message = {
-          type: "text",
-          text: event.message["text"]
-        }
-        client.reply_message(event["replyToken"], message)
-      end
+    event = params[:events][0]
+    event_type = event[:type]
+    reply_token = event[:replyToken]
+    output_text = ""
+    case event_type
+    when "message"
+      input_text = event[:message][:text]
+      output_text = input_text
+      message = {
+        type: "text",
+        text: output_text
+      }
+      client.reply_message(reply_token, message)
     end
+
+    # body = request.body.read
+    # events = client.parse_events_from(body)
+    # events.each do |event|
+    #   case event
+    #   when Line::Bot::Event::Message
+    #     message = {
+    #       type: "text",
+    #       text: event.message["text"]
+    #     }
+    #     client.reply_message(event["replyToken"], message)
+    #   end
+    # end
 
     render json: {}, status: :ok
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  root 'responses#index'
   post '/callback' => 'responses#callback'
-  resources :responses, only: :index
 end
