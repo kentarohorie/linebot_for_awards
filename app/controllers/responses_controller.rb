b532a3cf5c040417ad63fc91e7a94a7fb7f85239
kentarohorie <kentaro.horie.4@gmail.com>
2017-02-04 16:35:52 +0900

    change

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -3,16 +3,16 @@ class ResponsesController < ApplicationController
   CHANNEL_ACCESS_TOKEN = "V1nZVaGmdYgECu+jOw5My+tbeV0DoApp5d1cPsA5cg2YqOLkLxOD6Bd4TiCFjF4vj6VyTlaS9+S+a3qY+Zpr5YJOLkABHyqhv+7W38IO2+z4cTPQeFTCp3BK2aVOTRxuIVVIP8sF8sXZbUF7PI0xtgdB04t89/1O/w1cDnyilFU="
 
   def callback
-    event = params[:events][0]
-    event_type = event[:type]
-    reply_token = event[:replyToken]
-    output_text = ""
-    case event_type
-    when "message"
-      input_text = event[:message][:text]
-      output_text = input_text
-      client.reply_message(replyToken, output_text)
-    end
+    # event = params[:events][0]
+    # event_type = event[:type]
+    # reply_token = event[:replyToken]
+    # output_text = ""
+    # case event_type
+    # when "message"
+    #   input_text = event[:message][:text]
+    #   output_text = input_text
+    #   client.reply_message(replyToken, output_text)
+    # end
 
 
     render json: {}, status: :ok
