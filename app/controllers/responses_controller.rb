485f665c26cb8e7e78ea6e19998e29c6c510e775
kentarohorie <kentaro.horie.4@gmail.com>
2017-02-04 16:57:09 +0900

    change

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -3,26 +3,32 @@ class ResponsesController < ApplicationController
   CHANNEL_ACCESS_TOKEN = "V1nZVaGmdYgECu+jOw5My+tbeV0DoApp5d1cPsA5cg2YqOLkLxOD6Bd4TiCFjF4vj6VyTlaS9+S+a3qY+Zpr5YJOLkABHyqhv+7W38IO2+z4cTPQeFTCp3BK2aVOTRxuIVVIP8sF8sXZbUF7PI0xtgdB04t89/1O/w1cDnyilFU="
 
   def callback
-    event = params[:events][0]
-    event_type = event[:type]
-    reply_token = event[:replyToken]
-    puts "~=~=~=~=~~=~=~=~=~=~=~"
-    puts "~=~=~=~=~~=~=~=~=~=~=~"
-    puts event
-    puts event_type
-    puts reply_token
-    puts "~=~=~=~=~~=~=~=~=~=~=~"
-    puts "~=~=~=~=~~=~=~=~=~=~=~"
-    output_text = ""
-    case event_type
-    when "message"
-      input_text = event[:message][:text]
-      output_text = input_text
-      message = {
-        type: "text",
-        text: output_text
-      }
-      client.reply_message(reply_token, message)
+    # event = params[:events][0]
+    # event_type = event[:type]
+    # reply_token = event[:replyToken]
+    # output_text = ""
+    # case event_type
+    # when "message"
+    #   input_text = event[:message][:text]
+    #   output_text = input_text
+    #   message = {
+    #     type: "text",
+    #     text: output_text
+    #   }
+    #   client.reply_message(reply_token, message)
+    # end
+
+    body = request.body.read
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        message = {
+          type: "text",
+          text: event.message["text"]
+        }
+        client.reply_message(event["replyToken"], message)
+      end
     end
 
     render json: {}, status: :ok
