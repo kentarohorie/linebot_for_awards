f0d2e98d049ebb20405cd268c7fb4144bc7429e9
kentarohorie <kentaro.horie.4@gmail.com>
2017-02-04 17:32:19 +0900

    change

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -4,7 +4,6 @@ class ResponsesController < ApplicationController
   CHANNEL_ACCESS_TOKEN = "V1nZVaGmdYgECu+jOw5My+tbeV0DoApp5d1cPsA5cg2YqOLkLxOD6Bd4TiCFjF4vj6VyTlaS9+S+a3qY+Zpr5YJOLkABHyqhv+7W38IO2+z4cTPQeFTCp3BK2aVOTRxuIVVIP8sF8sXZbUF7PI0xtgdB04t89/1O/w1cDnyilFU="
 
   def callback
-    puts "hgeohgoehgoehogoehgoehgoehgoehgoehgoekentarohorie"
     event = params[:events][0]
     event_type = event[:type]
     reply_token = event[:replyToken]
@@ -17,21 +16,15 @@ class ResponsesController < ApplicationController
         type: "text",
         text: output_text
       }
+      puts "~=~=~=~=~=~=~="
+      puts event
+      puts event_type
+      puts reply_token
+      puts message
+      puts "~=~=~=~=~=~=~="
+
       client.reply_message(reply_token, message)
     end
-
-    # body = request.body.read
-    # events = client.parse_events_from(body)
-    # events.each do |event|
-    #   case event
-    #   when Line::Bot::Event::Message
-    #     message = {
-    #       type: "text",
-    #       text: event.message["text"]
-    #     }
-    #     client.reply_message(event["replyToken"], message)
-    #   end
-    # end
 
     render json: {}, status: :ok
   end
