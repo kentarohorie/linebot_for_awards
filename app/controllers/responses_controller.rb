b4b00fdf422842b917e204ca8780c3f369395cbc
kentarohorie <kentaro.horie.4@gmail.com>
2017-02-05 11:20:12 +0900

    change

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -16,24 +16,44 @@ class ResponsesController < ApplicationController
       reply_text(output_text, reply_token)
     elsif is_monster
       # reply_text("500 Internal Server Error が現れた！", reply_token)
+      # reply_content = {
+      #   type: "template",
+      #   altText: "button tamplate",
+      #   template: {
+      #     type: "buttons",
+      #     thumbnailImageUrl: "http://feelgoodokinawa1945.com/wp-content/uploads/2013/09/31ee6b4ce9b15e6ae110964569e333cb.jpg",
+      #     title: "Title",
+      #     text: "text",
+      #     actions: [
+      #       {
+      #         type: "postback",
+      #         label: "label",
+      #         data: "hogedata"
+      #       },
+      #       {
+      #         type: "postback",
+      #         label: "label",
+      #         data: "hogedata"
+      #       }
+      #     ]
+      #   }
+      # }
       reply_content = {
         type: "template",
-        altText: "button tamplate",
+        altText: "altText",
         template: {
-          type: "buttons",
-          thumbnailImageUrl: "http://feelgoodokinawa1945.com/wp-content/uploads/2013/09/31ee6b4ce9b15e6ae110964569e333cb.jpg",
-          title: "Title",
+          type: "confirm",
           text: "text",
           actions: [
             {
-              type: "postback",
+              type: "message",
               label: "label",
-              data: "hogedata"
+              text: "text"
             },
             {
-              type: "postback",
+              type: "message",
               label: "label",
-              data: "hogedata"
+              text: "text"
             }
           ]
         }
