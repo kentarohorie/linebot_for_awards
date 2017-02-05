a4c56abfcddc75cb67d20c389d84a1c32dc1d0e4
kentarohorie <kentaro.horie.4@gmail.com>
2017-02-05 10:11:13 +0900

    hge

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,7 @@
-every 6.hours do
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+set :output, 'log/cron.log'
+
+every 1.minute do
   User.all.each do |user|
     user.love = user.love - 1
     user.save
