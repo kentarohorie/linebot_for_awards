5fef13e0245b08befd124e22d0c99f627cd94289
kentarohorie <kentaro.horie.4@gmail.com>
2017-02-05 11:48:49 +0900

    change

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -14,34 +14,34 @@ class ResponsesController < ApplicationController
     if is_death(user)
       output_text = "あなたのnyaineは死んでしまいました。"
       reply_text(output_text, reply_token)
-    elsif params[:events][0][:postback][:data] == "panchi"
-      message = {
-        type: "image",
-        "originalContentUrl": "https://cyac.com/sites/default/files/teams/2006010210_1113818290.jpg",
-        "previewImageUrl": "https://cyac.com/sites/default/files/teams/2006010210_1113818290.jpg"
-      }
-      client.reply_message("#{reply_token}", message)
-      if rand(4) == 0
-        user.love = -100
-        user.save
-        reply_text("nyaineが攻撃をうけた！", reply_token)
-      else
-        reply_text("倒したにゃ！レベルアップにゃ〜♡", reply_token)
-      end
-    elsif params[:events][0][:postback][:data] == "sleep"
-      message = {
-        type: "image",
-        "originalContentUrl": "https://rr.img.naver.jp/mig?src=http%3A%2F%2Fpds.exblog.jp%2Fpds%2F1%2F200907%2F18%2F94%2Fd0128594_20534369.jpg&twidth=1000&theight=0&qlt=80&res_format=jpg&op=r",
-        "previewImageUrl": "https://rr.img.naver.jp/mig?src=http%3A%2F%2Fpds.exblog.jp%2Fpds%2F1%2F200907%2F18%2F94%2Fd0128594_20534369.jpg&twidth=1000&theight=0&qlt=80&res_format=jpg&op=r"
-      }
-      client.reply_message("#{reply_token}", message)
-      if rand(4) == 0
-        user.love = -100
-        user.save
-        reply_text("nyaineが攻撃をうけた！", reply_token)
-      else
-        reply_text("倒したにゃ！レベルアップにゃ〜♡", reply_token)
-      end
+    # elsif params[:events][0][:postback][:data] == "panchi"
+    #   message = {
+    #     type: "image",
+    #     originalContentUrl: "https://cyac.com/sites/default/files/teams/2006010210_1113818290.jpg",
+    #     previewImageUrl: "https://cyac.com/sites/default/files/teams/2006010210_1113818290.jpg"
+    #   }
+    #   client.reply_message("#{reply_token}", message)
+    #   if rand(4) == 0
+    #     user.love = -100
+    #     user.save
+    #     reply_text("nyaineが攻撃をうけた！", reply_token)
+    #   else
+    #     reply_text("倒したにゃ！レベルアップにゃ〜♡", reply_token)
+    #   end
+    # elsif params[:events][0][:postback][:data] == "sleep"
+    #   message = {
+    #     type: "image",
+    #     originalContentUrl: "https://rr.img.naver.jp/mig?src=http%3A%2F%2Fpds.exblog.jp%2Fpds%2F1%2F200907%2F18%2F94%2Fd0128594_20534369.jpg&twidth=1000&theight=0&qlt=80&res_format=jpg&op=r",
+    #     previewImageUrl: "https://rr.img.naver.jp/mig?src=http%3A%2F%2Fpds.exblog.jp%2Fpds%2F1%2F200907%2F18%2F94%2Fd0128594_20534369.jpg&twidth=1000&theight=0&qlt=80&res_format=jpg&op=r"
+    #   }
+    #   client.reply_message("#{reply_token}", message)
+    #   if rand(4) == 0
+    #     user.love = -100
+    #     user.save
+    #     reply_text("nyaineが攻撃をうけた！", reply_token)
+    #   else
+    #     reply_text("倒したにゃ！レベルアップにゃ〜♡", reply_token)
+    #   end
     elsif is_monster
       reply_content = {
         type: "template",
